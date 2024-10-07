572473340c153a3be7f7533431ed2d8168db120e
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-10-07 15:53:28 +0600

    four folder added

---
+++ 13-event-emitter.js
@@ -0,0 +1,11 @@
+const EvenEmitter = require("events");
+
+const customEmitter = new EvenEmitter();
+
+customEmitter.on("response", (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`);
+});
+customEmitter.on("response", () => {
+  console.log(`some other logic here `);
+});
+customEmitter.emit("response", "john", 34);
---
+++ 2-setTimeout.js
@@ -0,0 +1,7 @@
+//started operating system process
+console.log("first");
+setTimeout(() => {
+  console.log("secod");
+}, 0);
+console.log("third");
+// completed and exited operating system process
